ccab79f5ebdf327581d3c0638debf237d01140c3
Lars Weimer <l.weimer@elancer-team.de>
2018-06-25 15:54:50 +0200

    Adding needed files...

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php
-get_header(); ?>
+<?php get_header(); ?>
 
 <h1> Index! </h1>
+
+<?php get_footer(); ?>
---
+++ landingpage.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 25.06.2018
+ * Time: 15:54
+ */
